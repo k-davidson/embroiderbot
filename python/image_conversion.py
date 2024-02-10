27676590b06a1000053c8b34b5a64cf54450752d
Kelbie Davidson <kelbiedavidsonuq@gmail.com>
2024-02-10 17:28:52 +1100

    Bitmaps

--- python/image_conversion.py
+++ python/image_conversion.py
@@ -35,20 +35,35 @@ arr = np.asarray(resized)
 arr = arr[:, :, 0:3]
 arr = arr.reshape((-1, 3))
 
-kmeans = KMeans(n_clusters=args.colours, n_init='auto').fit(
+kmeans = KMeans(n_clusters=args.colours, init='k-means++', n_init='auto', random_state=42).fit(
     arr[(arr != [0, 0, 0]).any(axis=1)])
 centers = kmeans.cluster_centers_
 
 reduced = centers[kmeans.predict(arr)]
+
+centers = centers.astype('uint8')
+reduced = reduced.astype('uint8')
+
 reduced[arr == 0] = 0
 reduced = reduced.reshape(
     (img.height, img.width, 3)).astype('uint8')
 
-output_path = args.output_filename
-if output_path is None:
-    input_path = Path(args.image)
-    output_path = str(input_path.parent /
-                      (input_path.stem + f"_processed_{args.colours}{input_path.suffix}"))
+reduced_img = Image.fromarray(reduced, "RGB")
 
-img = Image.fromarray(reduced, "RGB")
-img.save(output_path)
+output_path = Path(args.image)
+processed_path = output_path.parent / \
+    (output_path.stem + f"_processed_{args.colours}" + output_path.suffix)
+
+reduced_img.save(str(processed_path))
+
+bitmap_dir = output_path.parent / f"{output_path.stem}_bitmaps"
+bitmap_dir.mkdir(parents=True, exist_ok=True)
+
+for center in centers:
+    bitmap = reduced.copy()
+    mask = np.all(bitmap == center, axis=2)
+    bitmap[mask] = [255, 255, 255]
+    bitmap[~mask] = [0, 0, 0]
+    bitmap_img = Image.fromarray(bitmap, "RGB")
+    bitmap_img.save(
+        str(bitmap_dir / f"{output_path.stem}_channel_{'_'.join([str(c) for c in center])}.png"))
